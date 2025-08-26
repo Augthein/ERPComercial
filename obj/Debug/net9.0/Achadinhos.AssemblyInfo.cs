2fc20ecc3bfb6c1a1bedbcc6c2894e1750d6e111
Augthein <augustus.g28@gmail.com>
2025-08-26 11:12:12 -0300

    Signed-off-by: Augusto Brito <vulgusstudios@gmail.com>

--- obj/Debug/net9.0/Achadinhos.AssemblyInfo.cs
+++ obj/Debug/net9.0/Achadinhos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Achadinhos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0016c2d4d22fbafea873f1cbfc5df40bbc6034")]
 [assembly: System.Reflection.AssemblyProductAttribute("Achadinhos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Achadinhos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
